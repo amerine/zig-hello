36604155ac5fcfe8cb95929b08629120f3783613
Mark Turner <mark.turner@salesforce.com>
2022-06-20 13:25:32 -0700

    pointers: zig has them, whew

--- src/main.zig
+++ src/main.zig
@@ -252,3 +252,25 @@ test "runtime safety: out of bounds" {
 //    const y: u32 = if (x == 2) 5 else unreachable;
 //    _ = y;
 //}
+
+
+fn increment(num: *u8) void {
+    num.* += 1;
+}
+
+test "pointers" {
+    var x: u8 = 1;
+    increment(&x);
+    try expect(x == 2);
+}
+
+//test "naughty pointer" {
+//    var x: u16 = 0;
+//    var y: *u8 = @intToPtr(*u8, x);
+//    _ = y;
+//}
+
+test "usize" {
+    try expect(@sizeOf(usize) == @sizeOf(*u8));
+    try expect(@sizeOf(isize) == @sizeOf(*u8));
+}
